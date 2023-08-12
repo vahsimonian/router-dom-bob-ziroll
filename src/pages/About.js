3cf184226c1f50cf1fed6d5bfe45e114d4b47ebe
vahsimonian <simonianvah@gmail.com>
2023-08-12 07:33:15 +0700

    commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 export default function About() {
   return (
     <div className='about-page-container'>
-      <img src={bgImg} className='about-hero-image' />
+      <img alt='hero-img' src={bgImg} className='about-hero-image' />
       <div className='about-page-content'>
         <h1>Don’t squeeze in a sedan when you could relax in a van.</h1>
         <p>
