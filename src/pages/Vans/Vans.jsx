2beef6766fe1733f4a600a67c39f65128241e6f0
vahsimonian <simonianvah@gmail.com>
2023-08-16 10:19:48 +0700

    commit

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -33,24 +33,6 @@ export default function Vans() {
     </Link>
   ))
 
-  {
-    /* to use the buttons comment FROM HERE  */
-  }
-
-  // const getNewSearchParamString = (key, value) => {
-  //   const sp = new URLSearchParams(searchParams)
-  //   if (value === null) {
-  //     sp.delete(key)
-  //   } else {
-  //     sp.set(key, value)
-  //   }
-  //   return `?${sp.toString()}`
-  // }
-
-  {
-    /* to use the buttons comment TO HERE  */
-  }
-
   function handleFilterChange(key, value) {
     setSearchParams((prevParams) => {
       if (value === null) {
@@ -66,36 +48,12 @@ export default function Vans() {
     <div className='van-list-container'>
       <h1>Explore our van options</h1>
 
-      {/* to use the buttons comment FROM HERE  */}
-
-      {/* <Link to={getNewSearchParamString('type', 'simple')} className='van-type'>
-        Simple
-      </Link>
-      <Link to={getNewSearchParamString('type', 'rugged')} className='van-type'>
-        Rugged
-      </Link>
-      <Link to={getNewSearchParamString('type', 'luxury')} className='van-type'>
-        Luxury
-      </Link>
-      <Link
-        to={getNewSearchParamString('type', null)}
-        className='van-type clear-filters'
-      >
-        clear filters
-      </Link> */}
-
-      {/* to use the buttons comment TO HERE  */}
-
-      {/* gap */}
-
-      {/* to use the links comment FROM HERE  */}
       <button
         className={
           typeFilter === 'simple'
             ? 'van-type simple selected'
             : `van-type simple`
         }
-        // onClick={() => setSearchParams({ type: 'simple' })}
         onClick={() => handleFilterChange('type', 'simple')}
       >
         simple
@@ -121,8 +79,6 @@ export default function Vans() {
         luxury
       </button>
       {searchParams.size > 0 ? (
-        // You can use one of these =)
-        // {typeFilter > 0 ? (
         <button
           className='van-type clear-filters'
           onClick={() => handleFilterChange('type', null)}
@@ -130,7 +86,6 @@ export default function Vans() {
           clear
         </button>
       ) : null}
-      {/* to use the links comment TO HERE  */}
 
       <div className='van-list'>{vanElements}</div>
     </div>
